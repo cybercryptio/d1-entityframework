c6cdd071532ea2c188747f4b765506f3557ad972
Kim Christensen <kim@cyber-crypt.com>
2022-04-26 10:42:21 +0200

    Add documentation comments

--- Encryptonize.EntityFramework/src/Migrator/EncryptonizeMigrator.cs
+++ Encryptonize.EntityFramework/src/Migrator/EncryptonizeMigrator.cs
@@ -1,23 +1,39 @@
 // Copyright 2020-2022 CYBERCRYPT
 
-using System.Linq.Expressions;
-using System.Text;
 using Encryptonize.Client;
 using Microsoft.EntityFrameworkCore;
 
 namespace Encryptonize.EntityFramework.Migrator;
 
+/// <summary>
+/// Migrator used to migrate unencrypted data to encrypted data.
+/// </summary>
+/// <typeparam name="TContext">The <see cref="DbContext"> migration is done on.</typeparam>
 public class EncryptonizeMigrator<TContext> where TContext : DbContext
 {
     private readonly TContext context;
     private readonly IEncryptonizeClient client;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EncryptonizeMigrator{TContext}"/> class.
+    /// </summary>
+    /// <param name="context">The <see cref="DbContext"> migration is done on.</param>
+    /// <param name="client">The <see cref="IEncryptonizeClient"> used to encrypt data.</param>
     public EncryptonizeMigrator(TContext context, IEncryptonizeClient client)
     {
         this.context = context;
         this.client = client;
     }
 
+    /// <summary>
+    /// Migrate data from a unencrypted column to an encrypted column.
+    /// </summary>
+    /// <param name="where">Filter to locate rows that should be migrated.</param>
+    /// <param name="oldPropertyGetter"><see cref="Func{T, TResult}" /> used to get the unecrypted value.</param>
+    /// <param name="newPropertySetter"><see cref="Action{T, TResult}" /> used to set the encrypted value.</param>
+    /// <param name="batchSize">The number of rows to process at a time.</param>
+    /// <typeparam name="TEntry">The type of entry to migrate.</typeparam>
+    /// <typeparam name="TValue">The type of value to migrate.</typeparam>
     public void Migrate<TEntry, TValue>(
         Func<TContext, IQueryable<TEntry>> where,
         Func<TEntry, TValue> oldPropertyGetter,
@@ -45,6 +61,7 @@ public class EncryptonizeMigrator<TContext> where TContext : DbContext
         }
     }
 
+    /// <inheritdoc cref="Migrate" />
     public async Task MigrateAsync<TEntry, TValue>(
         Func<TContext, IQueryable<TEntry>> where,
         Func<TEntry, TValue> oldPropertyGetter,
